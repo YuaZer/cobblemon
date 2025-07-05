bdb47532b949793c7eaeba6880ba41fd14cba797
Sterrezo <sterrezo@protonmail.com>
2025-07-05 18:50:31 +0200

    let's not import everything there is

--- common/src/main/kotlin/com/cobblemon/mod/common/block/HeartyGrainsBlock.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/block/HeartyGrainsBlock.kt
@@ -39,7 +39,7 @@ import net.minecraft.world.level.material.FluidState
 import net.minecraft.world.level.material.Fluids
 import net.minecraft.world.phys.shapes.CollisionContext
 import net.minecraft.world.phys.shapes.VoxelShape
-import net.minecraft.world.level.block.*
+import net.minecraft.world.level.block.SoundType
 
 @Suppress("OVERRIDE_DEPRECATION")
 class HeartyGrainsBlock(settings: Properties) : CropBlock(settings), SimpleWaterloggedBlock {
