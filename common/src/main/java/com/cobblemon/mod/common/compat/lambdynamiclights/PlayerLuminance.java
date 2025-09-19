a7bd70e334b61e4d798bbc740de152b88c367b43
Nadja Reitzenstein <me@dequbed.space>
2025-09-19 13:10:12 +0200

    Make only held Pokedexes cast light

--- common/src/main/java/com/cobblemon/mod/common/compat/lambdynamiclights/PlayerLuminance.java
+++ common/src/main/java/com/cobblemon/mod/common/compat/lambdynamiclights/PlayerLuminance.java
@@ -8,11 +8,16 @@
 
 package com.cobblemon.mod.common.compat.lambdynamiclights;
 
+import com.cobblemon.mod.common.Cobblemon;
 import com.cobblemon.mod.common.client.render.layer.PokemonOnShoulderRenderer;
 import dev.lambdaurora.lambdynlights.api.entity.luminance.EntityLuminance;
 import dev.lambdaurora.lambdynlights.api.item.ItemLightSourceManager;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -20,6 +25,7 @@ import org.jetbrains.annotations.Range;
 
 public final class PlayerLuminance extends CustomLuminance implements EntityLuminance {
     public static final PlayerLuminance INSTANCE = new PlayerLuminance();
+    private static final TagKey<Item> POKEDEX = TagKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath(Cobblemon.MODID, "pokedex"));
 
     @Override
     public @NotNull Type type() {
@@ -29,19 +35,27 @@ public final class PlayerLuminance extends CustomLuminance implements EntityLumi
     @Override
     public @Range(from = 0L, to = 15L) int getLuminance(@NotNull ItemLightSourceManager itemLightSourceManager, @NotNull Entity entity) {
         if (entity instanceof Player player) {
-            var leftShoulder = PokemonOnShoulderRenderer.shoulderDataFrom(player.getShoulderEntityLeft());
-            var rightShoulder = PokemonOnShoulderRenderer.shoulderDataFrom(player.getShoulderEntityRight());
+            final int shoulderLightLevel = shoulderLuminance(itemLightSourceManager, player);
 
-            if (leftShoulder == null && rightShoulder == null) {
-                return 0;
-            }
+            final int itemLightLevel = player.isHolding(stack -> stack.is(POKEDEX)) ? 13 : 0;
 
-            final int leftShoulderLightLevel = extractShoulderLightLevel(leftShoulder, itemLightSourceManager, player.isUnderWater());
-            final int rightShoulderLightLevel = extractShoulderLightLevel(rightShoulder, itemLightSourceManager, player.isUnderWater());
-            return Math.max(leftShoulderLightLevel, rightShoulderLightLevel);
+            return Math.max(shoulderLightLevel, itemLightLevel);
         } else {
             return 0;
         }
+    }
+
+    private @Range(from = 0L, to = 15L) static int shoulderLuminance(@NotNull ItemLightSourceManager itemLightSourceManager, @NotNull Player player) {
+        final var leftShoulder = PokemonOnShoulderRenderer.shoulderDataFrom(player.getShoulderEntityLeft());
+        final var rightShoulder = PokemonOnShoulderRenderer.shoulderDataFrom(player.getShoulderEntityRight());
+
+        if (leftShoulder == null && rightShoulder == null) {
+            return 0;
+        }
+
+        final int leftShoulderLightLevel = extractShoulderLightLevel(leftShoulder, itemLightSourceManager, player.isUnderWater());
+        final int rightShoulderLightLevel = extractShoulderLightLevel(rightShoulder, itemLightSourceManager, player.isUnderWater());
+        return Math.max(leftShoulderLightLevel, rightShoulderLightLevel);
     }
 
     private static int extractShoulderLightLevel(@Nullable PokemonOnShoulderRenderer.ShoulderData shoulderData, @NotNull ItemLightSourceManager itemLightSourceManager, boolean underwater) {
