2eb4062f67016e741abe4c0512da902bdd6d6a3b
Polymeta <40173118+Polymeta@users.noreply.github.com>
2025-09-22 10:23:14 +0100

    [skip-ci] re-add client config for consistency
    Signed-off-by: Polymeta <40173118+Polymeta@users.noreply.github.com>

--- gradle/build-logic/src/main/kotlin/cobblemon.platform-conventions.gradle.kts
+++ gradle/build-logic/src/main/kotlin/cobblemon.platform-conventions.gradle.kts
@@ -17,9 +17,9 @@ loom {
     val clientConfig = runConfigs.getByName("client")
     clientConfig.runDir = "runClient"
 //    clientConfig.vmArg("-Dmixin.debug=true")
-//    clientConfig.programArg("--username=AshKetchum")
+    clientConfig.programArg("--username=AshKetchum")
     //This is AshKetchum's UUID so you get an Ash Ketchum skin
-//    clientConfig.programArg("--uuid=93e4e551-589a-41cb-ab2d-435266c8e035")
+    clientConfig.programArg("--uuid=93e4e551-589a-41cb-ab2d-435266c8e035")
     val serverConfig = runConfigs.getByName("server")
     serverConfig.runDir = "runServer"
 
